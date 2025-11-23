fe73c3b1dfca409aa688b5937855fb094e88619a
Serghei Matenco <56189467+Revo69@users.noreply.github.com>
2025-11-24 01:08:23 +0200

    Update links_loader.py

--- pipeline/acquisition/links_loader.py
+++ pipeline/acquisition/links_loader.py
@@ -44,8 +44,8 @@ def init_driver():
     options = Options()
     options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    #options.add_argument("--no-sandbox")
+    #options.add_argument("--disable-dev-shm-usage")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     return webdriver.Chrome(options=options)
 
