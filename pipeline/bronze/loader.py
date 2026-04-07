c212006127f650ddd5ce837f08b99101cda0d8d6
Serghei Matenсo <56189467+Revo69@users.noreply.github.com>
2026-04-07 09:23:43 +0300

    Update loader.py

--- pipeline/bronze/loader.py
+++ pipeline/bronze/loader.py
@@ -4,7 +4,10 @@ import logging
 import uuid
 import argparse
 from dotenv import load_dotenv
-from .parsers import parse_features
+
+#from .parsers import parse_features
+from .parsers_old import parse_features
+
 from supabase import create_client, Client
 from selenium.common.exceptions import TimeoutException, WebDriverException
 import undetected_chromedriver as uc
